7df4fb773519ce6eb77ec6792b3400b60c5d9a22
Shane Watters <shane.watters.nz@gmail.com>
2017-05-05 14:16:47 +1200

    updating collect.sh to retrieve images from a different site and rename all files retrieved to remove the query strings

--- collect.sh
+++ collect.sh
@@ -9,14 +9,14 @@ mkdir -p downloads
 # clean the directory if there are old results
 rm -f downloads/*
 
-SEARCH_STRING="garfield"
+SEARCH_STRING="fruit"
 
-# get 5 pages
-for PAGE in {1..5}
+# get 20 pages
+for PAGE in {1..20}
 do
     # build the url
-    URL='http://www.trademe.co.nz/Browse/SearchResults.aspx?&cid=0&searchType=&searchString='$SEARCH_STRING'&x=0&y=0&type=Search&sort_order=&redirectFromAll=False&rptpath=all&page='$PAGE'&user_region=100&user_district=0&generalSearch_keypresses=8&generalSearch_suggested=0&generalSearch_suggestedCategory='
-
+    URL='http://www.sluniverse.com/snapzilla/Home/Search?term='$SEARCH_STRING'&page='$PAGE'#pictures'
+    
     # fetch the images
     wget --adjust-extension \
          --random-wait \
@@ -29,7 +29,7 @@ do
          --page-requisites \
          --directory-prefix=downloads \
          --execute robots=off \
-         --accept=[0-9]*.jpg \
+         --accept=*jpg*,*png* \
          $URL
 
 # other unused arguments
@@ -38,3 +38,17 @@ do
          # --domains en.wikipedia.org \
      
 done
+
+#change to the downloads directory
+cd downloads
+
+#remove query strings from all jpg files
+for file in *.jpg\?*; do mv "$file" "${file%%\?*}"; done
+
+#remove query strings from all png files
+for file in *.png\?*; do mv "$file" "${file%%\?*}"; done
+
+#return to previous directory
+cd ../
+
+# URL='http://www.trademe.co.nz/Browse/SearchResults.aspx?&cid=0&searchType=&searchString='$SEARCH_STRING'&x=0&y=0&type=Search&sort_order=&redirectFromAll=False&rptpath=all&page='$PAGE'&user_region=100&user_district=0&generalSearch_keypresses=8&generalSearch_suggested=0&generalSearch_suggestedCategory='
